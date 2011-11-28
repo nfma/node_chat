08e00f6bf314983609b3d3de5ec8d03f54bd9647
Nuno Marques <nuno@platogo.com>
2011-11-28 16:09:02 +0100

    using serious beats app db (postgres)

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-var mysql = require('mysql'),
-    DATABASE = 'serious_beats',
-    client = mysql.createClient({user:'serious',password:'beats',host:'localhost',port:3306,database:'serious_beats'});
+var pg = require('pg'),
+    db_config = process.env.DATABASE_URL || {user:'serious',database:'serious_beats',password:'beats'}
+    client = new pg.Client(db_config);
+
+client.connect();
 
 var db = exports,
     sys = require("sys");
 
 db.log = function(user, text, time, type, total_users) {
-  client.query('INSERT INTO logs values (NULL, ?, ?, ?, ?, ?)',
+  client.query('INSERT INTO logs (name, text, type, time, total_users) values ($1, $2, $3, $4, $5)',
       [user, text, type, time, total_users], 
       function(error) {
         if (error)
