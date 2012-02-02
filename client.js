2b9b894dda2b6fc8880297d51b3990625e0734b9
Nuno Marques <nuno@platogo.com>
2012-02-02 16:09:17 +0100

    fixed typo in the host domain of the main app

--- client.js
+++ client.js
@@ -513,5 +513,5 @@ function goTo(url) {
 
 function appUrl() {
   app = window.location.href;
-  return app.search('localhost') == - 1 ? 'https://www.your-turn.fm/' : 'http://localhost:3000/';
+  return app.search('localhost') == - 1 ? 'https://www.yourturn.fm/' : 'http://localhost:3000/';
 }
